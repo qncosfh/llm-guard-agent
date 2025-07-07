05838f4024f167746ce61050408deb0bf61b12e4
qncosfh <96825334+qncosfh@users.noreply.github.com>
2025-07-07 15:26:01 +0800

    Update config.go

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,16 +1,20 @@
 package config
 
 import (
-	"gopkg.in/yaml.v3"
 	"os"
+
+	"gopkg.in/yaml.v3"
 )
 
 type AppConfig struct {
-	Listen    string `yaml:"listen"`
+	Listen    string `yaml:"Listen"`
+	WebListen string `yaml:"WebListen"`
 	ModelURL  string `yaml:"model_url"`
 	ModelName string `yaml:"model_name"`
 	ApiKey    string `yaml:"api_key"`
 	RulesFile string `yaml:"rules_file"`
+	Platform  string `yaml:"platform"`
+	OnnxURL   string `yaml:"onnx_url"`
 }
 
 var Cfg AppConfig
